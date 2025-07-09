6df139920db1d80fb153f9023981abc703e6bbc6
Noah Almeida <noahjalmeida@gmail.com>
2025-07-09 13:21:17 -0400

    Initial Models

--- Bangalore Dataset/XGBoostMode.py
+++ Bangalore Dataset/XGBoostMode.py
@@ -5,6 +5,7 @@ from CatFunc import categorize_congestion, categorize_signals
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+import shap
 
 df = pd.read_csv('/Users/daddy/Downloads/Banglore_traffic_Dataset.csv')
 print(df.head())
@@ -17,7 +18,7 @@ df['Weather_Encoded'] = le_weather.fit_transform(df['Weather Conditions'])
 #le_roadwork = LabelEncoder()
 #df['Roadwork_Encoded'] = le_roadwork.fit_transform(df['Roadwork and Construction Activity'])
 
-X = df[['Traffic Volume', 'Average Speed', 'Congestion Level', 'Weather_Encoded', 'Incident Reports', 'Pedestrian and Cyclist Count', '']]
+X = df[['Traffic Volume', 'Average Speed', 'Congestion Level', 'Weather_Encoded', 'Incident Reports', 'Pedestrian and Cyclist Count']]
 y = df['TrafficCategory']
 
 le_target = LabelEncoder()
@@ -26,8 +27,8 @@ y_fit = le_target.fit_transform(y)
 X_train, X_test, y_train, y_test = train_test_split(X, y_fit, test_size=0.2, random_state=42)
 
 scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+X_train = pd.DataFrame(scaler.fit_transform(X_train), columns=X.columns)
+X_test = pd.DataFrame(scaler.transform(X_test), columns=X.columns)
 
 model = xgb.XGBClassifier(eta=0.0094,
                           n_estimators=200,
@@ -43,7 +44,8 @@ y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy * 100:.2f}%")
 
-xgb.plot_importance(model, importance_type='gain')
-plt.show()
+#explainer = shap.Explainer(model, X_train)
+#shap_values = explainer(X_test)
+#shap.summary_plot(shap_values[:, :, 2], X_test, feature_names=X_test.columns)
 
 
--- Smart Traffic Management Dataset/XGBoostModel3.py
+++ Smart Traffic Management Dataset/XGBoostModel3.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-import shap
 
 
 df = pd.read_csv('/Users/daddy/Downloads/smart_traffic_management_dataset.csv')
@@ -25,8 +24,8 @@ y_fit = le_target.fit_transform(y)
 X_train, X_test, y_train, y_test = train_test_split(X, y_fit, test_size=0.2, random_state=42)
 
 scaler = StandardScaler()
-X_train = pd.DataFrame(scaler.fit_transform(X_train), columns=X.columns)
-X_test = pd.DataFrame(scaler.transform(X_test), columns=X.columns)
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 model = xgb.XGBClassifier(eta = .001,
                           n_estimators= 200,
@@ -41,7 +40,6 @@ y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy * 100:.2f}%")
 
+xgb.plot_importance(model, importance_type='gain')
+plt.show()
 
-explainer = shap.Explainer(model, X_train)
-shap_values = explainer(X_test)
-shap.summary_plot(shap_values[:, :, 2], X_test, feature_names=X_test.columns)
